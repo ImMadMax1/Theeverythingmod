6f6aad20b5b24bf452a4495ce6a022b62117085f
ImMadMax1 <Max@DESKTOP-QBVTU1L.mshome.net>
2020-06-02 00:51:17 +0100

    Finalised Title block and added achievment on right click

---
+++ src/main/java/net/mcreator/theeverythingmod/procedures/TheEverythingModTitleBlockNeighbourBlockChangesProcedure.java
@@ -0,0 +1,31 @@
+package net.mcreator.theeverythingmod.procedures;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.entity.effect.LightningBoltEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.theeverythingmod.TheEverythingModModElements;
+
+@TheEverythingModModElements.ModElement.Tag
+public class TheEverythingModTitleBlockNeighbourBlockChangesProcedure extends TheEverythingModModElements.ModElement {
+	public TheEverythingModTitleBlockNeighbourBlockChangesProcedure(TheEverythingModModElements instance) {
+		super(instance, 3);
+	}
+
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			System.err.println("Failed to load dependency entity for procedure TheEverythingModTitleBlockNeighbourBlockChanges!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			System.err.println("Failed to load dependency world for procedure TheEverythingModTitleBlockNeighbourBlockChanges!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		World world = (World) dependencies.get("world");
+		if (world instanceof ServerWorld)
+			((ServerWorld) world).addLightningBolt(
+					new LightningBoltEntity(world, (int) (entity.getPosX()), (int) (entity.getPosY()), (int) (entity.getPosZ()), false));
+	}
+}
