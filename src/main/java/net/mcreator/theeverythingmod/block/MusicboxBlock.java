4cd20a1ad67033d8d338e230323a64c673868c2d
pudgemagee <whatp@pudgespc.mshome.net>
2020-06-02 00:59:42 +0100

    MUSIC BOX ADDED

---
+++ src/main/java/net/mcreator/theeverythingmod/block/MusicboxBlock.java
@@ -0,0 +1,62 @@
+
+package net.mcreator.theeverythingmod.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.theeverythingmod.TheEverythingModModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@TheEverythingModModElements.ModElement.Tag
+public class MusicboxBlock extends TheEverythingModModElements.ModElement {
+	@ObjectHolder("the_everything_mod:musicbox")
+	public static final Block block = null;
+	public MusicboxBlock(TheEverythingModModElements instance) {
+		super(instance, 3);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.MISCELLANEOUS).sound(SoundType.GLASS).hardnessAndResistance(2.5f, 10f).lightValue(0));
+			setRegistryName("musicbox");
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public void addInformation(ItemStack itemstack, IBlockReader world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("tunes nigga"));
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
