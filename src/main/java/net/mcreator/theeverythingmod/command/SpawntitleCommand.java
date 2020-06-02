c611bfa22e03fe1a1a86aee1c69a5695d7c00b6d
pudgemagee <whatp@pudgespc.mshome.net>
2020-06-02 01:28:54 +0100

    sync

--- src/main/java/net/mcreator/theeverythingmod/command/SpawntitleCommand.java
+++ src/main/java/net/mcreator/theeverythingmod/command/SpawntitleCommand.java
@@ -1,9 +1,25 @@
 
 package net.mcreator.theeverythingmod.command;
 
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.entity.Entity;
+import net.minecraft.command.Commands;
+import net.minecraft.command.CommandSource;
+
+import net.mcreator.theeverythingmod.procedures.SpawntitleblockProcedure;
+import net.mcreator.theeverythingmod.TheEverythingModModElements;
+
+import java.util.HashMap;
+import java.util.Arrays;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.arguments.StringArgumentType;
+
 @TheEverythingModModElements.ModElement.Tag
 public class SpawntitleCommand extends TheEverythingModModElements.ModElement {
-
 	public SpawntitleCommand(TheEverythingModModElements instance) {
 		super(instance, 5);
 	}
@@ -25,7 +41,6 @@ public class SpawntitleCommand extends TheEverythingModModElements.ModElement {
 			int y = entity.getPosition().getY();
 			int z = entity.getPosition().getZ();
 			World world = entity.world;
-
 			HashMap<String, String> cmdparams = new HashMap<>();
 			int[] index = {-1};
 			Arrays.stream(ctx.getInput().split("\\s+")).forEach(param -> {
@@ -33,19 +48,15 @@ public class SpawntitleCommand extends TheEverythingModModElements.ModElement {
 					cmdparams.put(Integer.toString(index[0]), param);
 				index[0]++;
 			});
-
 			{
 				java.util.HashMap<String, Object> $_dependencies = new java.util.HashMap<>();
 				$_dependencies.put("x", x);
 				$_dependencies.put("y", y);
 				$_dependencies.put("z", z);
 				$_dependencies.put("world", world);
-
 				SpawntitleblockProcedure.executeProcedure($_dependencies);
 			}
 		}
-
 		return 0;
 	}
-
 }
