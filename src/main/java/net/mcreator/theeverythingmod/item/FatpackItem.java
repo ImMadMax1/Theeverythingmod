255f55c78d5a0334d6fa12c86c515b9ca3175608
pudgemagee <whatp@pudgespc.mshome.net>
2020-06-02 18:43:07 +0100

    fatpack test gui

--- src/main/java/net/mcreator/theeverythingmod/item/FatpackItem.java
+++ src/main/java/net/mcreator/theeverythingmod/item/FatpackItem.java
@@ -2,19 +2,43 @@
 package net.mcreator.theeverythingmod.item;
 
 import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.fml.network.NetworkHooks;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.capabilities.Capability;
 
 import net.minecraft.world.World;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Direction;
+import net.minecraft.util.ActionResult;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.block.BlockState;
 
 import net.mcreator.theeverythingmod.itemgroup.EverythingmodItemGroup;
+import net.mcreator.theeverythingmod.gui.FatpackguiGui;
 import net.mcreator.theeverythingmod.TheEverythingModModElements;
 
+import javax.annotation.Nullable;
+import javax.annotation.Nonnull;
+
 import java.util.List;
+
+import io.netty.buffer.Unpooled;
 
 @TheEverythingModModElements.ModElement.Tag
 public class FatpackItem extends TheEverythingModModElements.ModElement {
@@ -53,6 +77,76 @@ public class FatpackItem extends TheEverythingModModElements.ModElement {
 		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
 			super.addInformation(itemstack, world, list, flag);
 			list.add(new StringTextComponent("a backpack for storage"));
+		}
+
+		@Override
+		public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity entity, Hand hand) {
+			ActionResult<ItemStack> ar = super.onItemRightClick(world, entity, hand);
+			ItemStack itemstack = ar.getResult();
+			int x = (int) entity.getPosX();
+			int y = (int) entity.getPosY();
+			int z = (int) entity.getPosZ();
+			if (entity instanceof ServerPlayerEntity) {
+				NetworkHooks.openGui((ServerPlayerEntity) entity, new INamedContainerProvider() {
+					@Override
+					public ITextComponent getDisplayName() {
+						return new StringTextComponent("Fatpack");
+					}
+
+					@Override
+					public Container createMenu(int id, PlayerInventory inventory, PlayerEntity player) {
+						PacketBuffer packetBuffer = new PacketBuffer(Unpooled.buffer());
+						packetBuffer.writeBlockPos(new BlockPos(x, y, z));
+						packetBuffer.writeByte(hand == Hand.MAIN_HAND ? 0 : 1);
+						return new FatpackguiGui.GuiContainerMod(id, inventory, packetBuffer);
+					}
+				}, buf -> {
+					buf.writeBlockPos(new BlockPos(x, y, z));
+					buf.writeByte(hand == Hand.MAIN_HAND ? 0 : 1);
+				});
+			}
+			return ar;
+		}
+
+		@Override
+		public ICapabilityProvider initCapabilities(ItemStack stack, @Nullable CompoundNBT compound) {
+			return new InventoryCapability();
+		}
+	}
+
+	private static class InventoryCapability implements ICapabilitySerializable<CompoundNBT> {
+		private final LazyOptional<ItemStackHandler> inventory = LazyOptional.of(this::createItemHandler);
+		@Override
+		public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> capability, @Nullable Direction side) {
+			return capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY ? this.inventory.cast() : LazyOptional.empty();
+		}
+
+		@Override
+		public CompoundNBT serializeNBT() {
+			return getItemHandler().serializeNBT();
+		}
+
+		@Override
+		public void deserializeNBT(CompoundNBT nbt) {
+			getItemHandler().deserializeNBT(nbt);
+		}
+
+		private ItemStackHandler createItemHandler() {
+			return new ItemStackHandler(1) {
+				@Override
+				public int getSlotLimit(int slot) {
+					return 64;
+				}
+
+				@Override
+				public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
+					return stack.getItem() != block;
+				}
+			};
+		}
+
+		private ItemStackHandler getItemHandler() {
+			return inventory.orElseThrow(RuntimeException::new);
 		}
 	}
 }
