3b147ca055b9ce3653cad9268b8683626f3335d8
ImMadMax1 <Max@DESKTOP-QBVTU1L.mshome.net>
2020-06-02 20:45:27 +0100

    easy stone, grass added

---
+++ src/main/java/net/mcreator/theeverythingmod/block/EasyGrassBlock.java
@@ -0,0 +1,58 @@
+
+package net.mcreator.theeverythingmod.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.util.Direction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.theeverythingmod.itemgroup.EverythingmodItemGroup;
+import net.mcreator.theeverythingmod.TheEverythingModModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@TheEverythingModModElements.ModElement.Tag
+public class EasyGrassBlock extends TheEverythingModModElements.ModElement {
+	@ObjectHolder("the_everything_mod:easy_grass")
+	public static final Block block = null;
+	public EasyGrassBlock(TheEverythingModModElements instance) {
+		super(instance, 16);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(EverythingmodItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ORGANIC).sound(SoundType.WET_GRASS).hardnessAndResistance(0.7f, 5.5f).lightValue(0));
+			setRegistryName("easy_grass");
+		}
+
+		@OnlyIn(Dist.CLIENT)
+		public boolean isSideInvisible(BlockState state, BlockState adjacentBlockState, Direction side) {
+			return adjacentBlockState.getBlock() == this ? true : super.isSideInvisible(state, adjacentBlockState, side);
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(Blocks.GRASS_BLOCK, (int) (1)));
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/theeverythingmod/block/EasystoneBlock.java
@@ -0,0 +1,52 @@
+
+package net.mcreator.theeverythingmod.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.theeverythingmod.itemgroup.EverythingmodItemGroup;
+import net.mcreator.theeverythingmod.TheEverythingModModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@TheEverythingModModElements.ModElement.Tag
+public class EasystoneBlock extends TheEverythingModModElements.ModElement {
+	@ObjectHolder("the_everything_mod:easystone")
+	public static final Block block = null;
+	public EasystoneBlock(TheEverythingModModElements instance) {
+		super(instance, 17);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(EverythingmodItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(0.8999999999999999f, 9f).lightValue(0)
+					.harvestLevel(1).harvestTool(ToolType.PICKAXE));
+			setRegistryName("easystone");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(Blocks.STONE, (int) (1)));
+		}
+	}
+}
