b96495a2b3c61760bd90166024f47ae2f59fd99a
ImMadMax1 <Max@DESKTOP-QBVTU1L.mshome.net>
2020-06-02 19:26:11 +0100

    mountain dew add

--- src/main/java/net/mcreator/theeverythingmod/procedures/FatpackItemInInventoryTickProcedure.java
+++ src/main/java/net/mcreator/theeverythingmod/procedures/FatpackItemInInventoryTickProcedure.java
@@ -1,11 +1,16 @@
 package net.mcreator.theeverythingmod.procedures;
 
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.theeverythingmod.TheEverythingModModElements;
+
 @TheEverythingModModElements.ModElement.Tag
 public class FatpackItemInInventoryTickProcedure extends TheEverythingModModElements.ModElement {
-
 	public FatpackItemInInventoryTickProcedure(TheEverythingModModElements instance) {
 		super(instance, 10);
-
 	}
 
 	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
@@ -13,12 +18,8 @@ public class FatpackItemInInventoryTickProcedure extends TheEverythingModModElem
 			System.err.println("Failed to load dependency entity for procedure FatpackItemInInventoryTick!");
 			return;
 		}
-
 		Entity entity = (Entity) dependencies.get("entity");
-
 		if (entity instanceof LivingEntity)
 			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.MINING_FATIGUE, (int) 1, (int) 0));
-
 	}
-
 }
