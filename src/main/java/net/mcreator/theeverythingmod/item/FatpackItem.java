b6083b8efba8ed5db109e49d5a0749d6f6ba7fb4
pudgemagee <whatp@pudgespc.mshome.net>
2020-06-02 18:39:56 +0100

    add fatpack

---
+++ src/main/java/net/mcreator/theeverythingmod/item/FatpackItem.java
@@ -0,0 +1,58 @@
+
+package net.mcreator.theeverythingmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.theeverythingmod.itemgroup.EverythingmodItemGroup;
+import net.mcreator.theeverythingmod.TheEverythingModModElements;
+
+import java.util.List;
+
+@TheEverythingModModElements.ModElement.Tag
+public class FatpackItem extends TheEverythingModModElements.ModElement {
+	@ObjectHolder("the_everything_mod:fatpack")
+	public static final Item block = null;
+	public FatpackItem(TheEverythingModModElements instance) {
+		super(instance, 8);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(EverythingmodItemGroup.tab).maxStackSize(1));
+			setRegistryName("fatpack");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("a backpack for storage"));
+		}
+	}
+}
