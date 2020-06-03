a299b4364ec7e5e99c53737e6ef576b49bfdbc47
pudgemagee <whatp@pudgespc.mshome.net>
2020-06-03 21:38:01 +0100

    violet gem made

---
+++ src/main/java/net/mcreator/theeverythingmod/block/VioletoreOreBlockBlock.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.theeverythingmod.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.theeverythingmod.itemgroup.EverythingmodItemGroup;
+import net.mcreator.theeverythingmod.TheEverythingModModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@TheEverythingModModElements.ModElement.Tag
+public class VioletoreOreBlockBlock extends TheEverythingModModElements.ModElement {
+	@ObjectHolder("the_everything_mod:violetore_ore_block")
+	public static final Block block = null;
+	public VioletoreOreBlockBlock(TheEverythingModModElements instance) {
+		super(instance, 21);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(EverythingmodItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(5f, 10f).lightValue(0).harvestLevel(13)
+					.harvestTool(ToolType.PICKAXE));
+			setRegistryName("violetore_ore_block");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/theeverythingmod/item/VioletoreGemItem.java
@@ -0,0 +1,66 @@
+
+package net.mcreator.theeverythingmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.theeverythingmod.itemgroup.EverythingmodItemGroup;
+import net.mcreator.theeverythingmod.TheEverythingModModElements;
+
+import java.util.List;
+
+@TheEverythingModModElements.ModElement.Tag
+public class VioletoreGemItem extends TheEverythingModModElements.ModElement {
+	@ObjectHolder("the_everything_mod:violetore_gem")
+	public static final Item block = null;
+	public VioletoreGemItem(TheEverythingModModElements instance) {
+		super(instance, 22);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(EverythingmodItemGroup.tab).maxStackSize(64));
+			setRegistryName("violetore_gem");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("can create magical items"));
+		}
+	}
+}
