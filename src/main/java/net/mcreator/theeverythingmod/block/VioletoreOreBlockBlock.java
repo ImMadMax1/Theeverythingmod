f4631ed3e992e31d7f6bf4d5b71624f591549396
pudgemagee <whatp@pudgespc.mshome.net>
2020-06-03 21:44:24 +0100

    violet ore blaock added

--- src/main/java/net/mcreator/theeverythingmod/block/VioletoreOreBlockBlock.java
+++ src/main/java/net/mcreator/theeverythingmod/block/VioletoreOreBlockBlock.java
@@ -1,17 +1,38 @@
 
 package net.mcreator.theeverythingmod.block;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.Minecraft;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.theeverythingmod.itemgroup.EverythingmodItemGroup;
+import net.mcreator.theeverythingmod.TheEverythingModModElements;
+
+import java.util.Random;
+import java.util.List;
+import java.util.Collections;
 
 @TheEverythingModModElements.ModElement.Tag
 public class VioletoreOreBlockBlock extends TheEverythingModModElements.ModElement {
-
 	@ObjectHolder("the_everything_mod:violetore_ore_block")
 	public static final Block block = null;
-
 	public VioletoreOreBlockBlock(TheEverythingModModElements instance) {
 		super(instance, 21);
-
 	}
 
 	@Override
@@ -20,15 +41,10 @@ public class VioletoreOreBlockBlock extends TheEverythingModModElements.ModEleme
 		elements.items
 				.add(() -> new BlockItem(block, new Item.Properties().group(EverythingmodItemGroup.tab)).setRegistryName(block.getRegistryName()));
 	}
-
 	public static class CustomBlock extends Block {
-
 		public CustomBlock() {
-			super(
-
-					Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(5f, 10f).lightValue(0).harvestLevel(13)
-							.harvestTool(ToolType.PICKAXE));
-
+			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(5f, 10f).lightValue(0).harvestLevel(3)
+					.harvestTool(ToolType.PICKAXE));
 			setRegistryName("violetore_ore_block");
 		}
 
@@ -40,6 +56,28 @@ public class VioletoreOreBlockBlock extends TheEverythingModModElements.ModEleme
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
 
+		@OnlyIn(Dist.CLIENT)
+		@Override
+		public void animateTick(BlockState state, World world, BlockPos pos, Random random) {
+			super.animateTick(state, world, pos, random);
+			PlayerEntity entity = Minecraft.getInstance().player;
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			int i = x;
+			int j = y;
+			int k = z;
+			if (true)
+				for (int l = 0; l < 4; ++l) {
+					double d0 = (i + random.nextFloat());
+					double d1 = (j + random.nextFloat());
+					double d2 = (k + random.nextFloat());
+					int i1 = random.nextInt(2) * 2 - 1;
+					double d3 = (random.nextFloat() - 0.5D) * 0.5D;
+					double d4 = (random.nextFloat() - 0.5D) * 0.5D;
+					double d5 = (random.nextFloat() - 0.5D) * 0.5D;
+					world.addParticle(ParticleTypes.COMPOSTER, d0, d1, d2, d3, d4, d5);
+				}
+		}
 	}
-
 }
